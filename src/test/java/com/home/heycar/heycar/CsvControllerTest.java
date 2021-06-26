ce2676d58f69aa01286f6a22aec41dbfd5e63734
Dmitry Arshinkin <arshinkinda@gmail.com>
2021-06-26 13:56:56 +0300

    added integration testo for rest api of upload cvc file

---
+++ src/test/java/com/home/heycar/heycar/CsvControllerTest.java
@@ -0,0 +1,39 @@
+package com.home.heycar.heycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import com.home.heycar.HeycarApplication;
+
+@SpringBootTest(classes = HeycarApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CsvControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void givenUploadForm_whenRequestWithMultipartData_thenSuccess() throws Exception {
+        WebTestClient client = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .build();
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder.part("file", new ClassPathResource("/test.csv"))
+                .contentType(MediaType.MULTIPART_FORM_DATA);
+        client.post()
+                .uri(uriBuilder -> uriBuilder.path("/v1/upload_csv")
+                        .queryParam("dealerId", "hello")
+                        .build())
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(multipartBodyBuilder.build()))
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody();
+    }
+}
